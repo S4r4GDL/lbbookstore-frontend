9feed42cf6624da86826d06bc7732e4982095c97
sarag <saragdl@gmail.com>
2024-04-22 00:37:03 -0300

    ADD - Create new component book-list-item

---
+++ src/app/components/products/books/book-list-item/book-list-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookListItemComponent } from './book-list-item.component';
+
+describe('BookListItemComponent', () => {
+  let component: BookListItemComponent;
+  let fixture: ComponentFixture<BookListItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BookListItemComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BookListItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
